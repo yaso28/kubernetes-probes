71e89995f56f8ea31a7707a7fc679f7d33574498
yaso28 <yaso28.f@gmail.com>
2022-05-16 18:27:36 +0900

    app-config: added error handlers

--- app-config/app.js
+++ app-config/app.js
@@ -4,6 +4,8 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+
+app.use(require('./middlewares/error-handler-404.js'));
+app.use(require('./middlewares/error-handler-500.js'));
 
 module.exports = app;

797ba4e34106cf94cdc07c9bdd7554c7f9c56324
yaso28 <yaso28.f@gmail.com>
2022-05-17 18:29:53 +0900

    app-target: modified base url for app-config

--- app-target/services/config-caller.js
+++ app-target/services/config-caller.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const get = async (url) => {
   await axios.get(url, {
-    baseURL: process.env.CONFIG_BASEURL || 'app-config',
+    baseURL: process.env.CONFIG_BASEURL || 'http://app-config:3000',
     params: {
       podname: process.env.HOSTNAME
     }
